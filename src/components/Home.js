dc4237f8e003b9e2e72b9fc64f20051d987c1359
Vincent <48564305+VncntDzn@users.noreply.github.com>
2020-08-14 09:03:20 +0800

    Fixed position of elements in homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,21 +7,24 @@ export default function Home() {
         position: "absolute",
         top: "240px",
         zIndex: 2,
-        marginLeft: "165px",
+        marginLeft: "80px",
+
     }
     return (
-        <div style={{ backgroundColor: "#45595A", display: "flex" }}>
-            <div style={{ border: "3px solid green", flex: "50%", }}>
+        <div style={{ backgroundColor: "#45595A", display: "flex", height: "685px" }}>
+            <div style={{ flex: "50%", }}>
                 <section style={h1Styles}>
-                    <h1 style={{ fontSize: "70px" }}>Get Fit.</h1>
-                    <h1 style={{ fontSize: "70px" }}>Don't Quit.</h1>
+                    <h1 style={{ fontSize: "70px", marginLeft: "130px" }}>Get Fit.</h1>
+                    <h1 style={{ fontSize: "70px", marginLeft: "130px" }}>Don't Quit.</h1>
+
+                    <div style={{ bottom: "350px", position: "relative", zIndex: "-1" }}>
+                        <img style={{ height: "650px", }} src={require('../assets/Group_4.png')} alt="Cross" />
+                    </div>
                 </section>
             </div>
-            <div style={{ marginLeft: "30px", position: "relative", zIndex: "1" }}>
-                <img style={{ height: "620px", }} src={require('../assets/Group_4.png')} alt="Cross" />
-            </div>
+
             <div style={{ flex: "50%" }}>
-                <img style={{ height: "620px", }} src={require('../assets/fit_person.png')} alt="Fit Person" />
+                <img style={{ marginRight: "-150px", height: "720px", }} src={require('../assets/fit_person.png')} alt="Fit Person" />
             </div>
         </div>
     )
