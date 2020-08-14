8336d6d3fae9fa71c9f7483d2448bcc4e81e236c
Vincent <48564305+VncntDzn@users.noreply.github.com>
2020-08-14 15:40:19 +0800

    Fixed position of fit person

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,15 +7,15 @@ export default function Home() {
         position: "absolute",
         top: "240px",
         zIndex: 2,
-        marginLeft: "80px",
+        marginLeft: "100px",
 
     }
     return (
-        <div style={{ backgroundColor: "#45595A", display: "flex", height: "675px", }}>
+        <div style={{ backgroundColor: "#45595A", display: "flex", height: "645px", }}>
             <div style={{ flex: "30%" }}>
                 <section style={h1Styles}>
-                    <h1 style={{ fontSize: "70px", marginLeft: "130px" }}>Get Fit.</h1>
-                    <h1 style={{ fontSize: "70px", marginLeft: "130px" }}>Don't Quit.</h1>
+                    <h1 style={{ fontSize: "70px", marginLeft: "140px" }}>Get Fit.</h1>
+                    <h1 style={{ fontSize: "70px", marginLeft: "140px" }}>Don't Quit.</h1>
 
                     <div style={{ bottom: "350px", position: "relative", zIndex: "-1" }}>
                         <img style={{ height: "650px", }} src={require('../assets/Group_4.png')} alt="Cross" />
@@ -23,9 +23,9 @@ export default function Home() {
                 </section>
             </div>
 
-            <div style={{ flex: "50%", position: "relative", overflow: "hidden" }}>
+            <div style={{ flex: "60%", position: "relative", overflow: "hidden" }}>
                 <div style={{ width: "45vw", position: "absolute", }}>
-                    <img style={{ height: "720px" }} src={require('../assets/fit_person.png')} alt="Fit Person" />
+                    <img style={{ height: "680px" }} src={require('../assets/fit_person.png')} alt="Fit Person" />
                 </div>
             </div>
         </div>
